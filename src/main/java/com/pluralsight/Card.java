0e42cfb4fb21abefa42c54e3483726e4b6f82fa5
Moubarak Congacou <cmouba1999@gmail.com>
2026-05-06 14:03:20 -0400

    Created 5 diferent classes for card, deck, hand, player and the Main, added getters and setters for the card class and added methods for the deck class

---
+++ src/main/java/com/pluralsight/Card.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Card {
+
+    private String suit;
+    private String value;
+
+
+    public Card(String value, String suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+
+
+    public void getPointValue(){
+
+    }
+
+
+
+
+}
---
+++ src/main/java/com/pluralsight/Deck.java
@@ -0,0 +1,59 @@
+package com.pluralsight;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+
+    /**
+     * An arrayList of cards
+     */
+    private ArrayList<Card> cards;
+
+
+    /**
+     * A class containing of elements of the arraylist (suits and values)
+     */
+    public Deck(){
+        cards = new ArrayList<>();
+        String[] suits = {"Hearts", "Spades", "Diamonds", "Clubs"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "Q", "K", "A"};
+
+
+        /**
+         *Creating all the cards in the deck and adding them to the arraylist
+        */
+
+        for (String suit: suits){
+            for (String value: values){
+                Card card = new Card(suit, value);
+                cards.add(card);
+
+            }
+        }
+
+    }
+
+
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+
+    public Card deal(){
+        //deal the top card (if there are many cards left
+        if (cards.size() > 0){
+            Card card = cards.remove(0);
+            return card;
+        }else {
+            return null;
+        }
+    }
+
+    public int getSize(){
+        return cards.size();
+    }
+
+
+
+
+}
